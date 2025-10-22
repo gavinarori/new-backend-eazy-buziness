08fedc96602338f4d883dc2aa2e4f872ccb8d709
gavinarori <arorigavin@gmail.com>
2025-10-22 13:09:46 +0300

    nofications working as expected

--- src/routes/notification.routes.ts
+++ src/routes/notification.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { listNotifications, markNotificationRead } from '../controllers/notification.controller';
+import { listNotifications, markNotificationRead, listComputedAlerts } from '../controllers/notification.controller';
 import { requireAuth } from '../middlewares/auth.middleware';
 
 export const notificationRouter = Router();
 
 notificationRouter.get('/', requireAuth, listNotifications);
 notificationRouter.post('/:id/read', requireAuth, markNotificationRead);
+notificationRouter.get('/alerts', requireAuth, listComputedAlerts);
 
 
